9c648d42163529077c4638a6c1cd0a3a54365db9
Szymon Gesicki <szym.gesicki@gmail.com>
2021-07-19 21:26:14 +0200

    Initial project setup

---
+++ SwiftExtensionsExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftExtensionsExamples
+//
+//  Created by Szymon Gęsicki on 19/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
